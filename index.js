071dbca2f49266de08e6ecbd26c33f4523e08f9f
KatriLindvall1 <lindvallkatri@hotmail.com>
2023-11-16 11:33:48 +0200

    fix

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
 const cors = require('cors')
-app.use(express.static('dist'))
-app.use(cors())
+
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -16,8 +15,11 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
+
 
 let notes = [
     {
